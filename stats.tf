9e2fb18ef1e0328ee677c7a8ae68fe4746ba8840
Wil Hunt <huntwj@gmail.com>
2015-12-05 01:33:02 -0800

    Add statsum to stat capture routing.

--- stats.tf
+++ stats.tf
@@ -10,20 +10,28 @@
 /def -Evar_user_capturing_46_stats -mregexp -t"^Your height is \d+ feet, \d+ inches, and you weigh \d+\.\d+ lbs\.$" wot_stats_captureVitals = \
     /test 1
 
-/def -Evar_user_capturing_46_stats -mregexp -t"^You are carrying \d+\.\d+ lbs and wearing \d+\.\d+ lbs, (very light)\.$" wot_stats_captureLoad = \
+/def -Evar_user_capturing_46_stats -mregexp -t"^You are carrying \d+\.\d+ lbs and wearing \d+\.\d+ lbs, (very light|fairly light)\.$" wot_stats_captureLoad = \
     /test 1
 
-/def -Evar_user_capturing_46_stats -mregexp -t"^Your base abilities are: Str:(\d+) Int:(\d+) Wil:(\d+) Dex:(\d+) Con:(\d+)\.$" wot_stats_captureStats = \
+/def -Evar_user_capturing_46_stats -ag -mregexp -t"^Your base abilities are: Str:(\d+) Int:(\d+) Wil:(\d+) Dex:(\d+) Con:(\d+)\.$" wot_stats_captureStats = \
+    /let _line=%{P0}%;\
     /let _str=%{P1}%;\
     /let _int=%{P2}%;\
     /let _wil=%{P3}%;\
     /let _dex=%{P4}%;\
     /let _con=%{P5}%;\
+    /let _sum=$[_str + _int + _wil + _dex + _con]%;\
     /util_setVar char.stats.str %{_str}%;\
     /util_setVar char.stats.int %{_int}%;\
     /util_setVar char.stats.wil %{_wil}%;\
     /util_setVar char.stats.dex %{_dex}%;\
-    /util_setVar char.stats.con %{_con}
+    /util_setVar char.stats.con %{_con}%;\
+    /util_setVar char.stats.sum %{_sum}%;\
+    /if (util_getVar("char.level") == 3) \
+        /echo -aCyellow %{_line} (Stat Sum: %{_sum})%;\
+    /else \
+        /echo %{_line} (Stat Sum: %{_sum})%;\
+    /endif
 
 /def -Evar_user_capturing_46_stats -mregexp -t"^Offensive bonus: \d+, Dodging bonus: \d+, Parrying bonus: \d+$" wot_stats_captureObDbPb = \
     /test 1
@@ -36,5 +44,5 @@
     /util_setVar capturing.stats 0
 
 /def -Evar_user_capturing_46_stats -p0 -mregexp -t"^.*$" wot_stats_captureUnknown = \
-    /echo Unknown stats line. Please address this.
+    /echo -aCred Unknown stats line. Please address this.
 
