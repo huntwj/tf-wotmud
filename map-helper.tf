bf75fd6ee3925c0bca08e8dc8c9b3a92e795423b
Wil Hunt <huntwj@gmail.com>
2015-12-07 00:45:24 -0800

    Update map-helper to match changes in tf-mapper

--- map-helper.tf
+++ map-helper.tf
@@ -5,10 +5,10 @@
 
 /require tf-mapper/main.tf
 
-/test mapper_mapFile := strcat(TF_NPM_ROOT, "/data/map.sqlite")
+/test map_mapFile := strcat(TF_NPM_ROOT, "/data/map.sqlite")
 
 /def -mregexp -t"^\[ obvious exits:( N)?( E)?( S)?( W)?( U)?( D)? \]$" wotmud_t_detectExits = \
-    /let _maxLines=10%;\
+    /let _maxLines=20%;\
     /let _lineNo=1%;\
     /let _roomDescription=%;\
     /while (_lineNo < _maxLines) \
@@ -17,9 +17,9 @@
         /test _lineNo := _lineNo + 1%;\
         /if (strstr(_encodedLine, "@{Ccyan}") == 0) \
             /let _roomName=%{_line}%;\
-            /test util_setVar("mapper.currentRoom.name", _roomName)%;\
-            /test util_setVar("mapper.currentRoom.description", _roomDescription)%;\
-            /test mapper_roomCaptured(_roomName, _roomDescription)%;\
+            /test util_setVar("map.currentRoom.name", _roomName)%;\
+            /test util_setVar("map.currentRoom.description", _roomDescription)%;\
+            /test map_roomCaptured(_roomName, _roomDescription)%;\
             /return%;\
         /else \
             /test _roomDescription := strcat(_line,"\10",_roomDescription)%;\
