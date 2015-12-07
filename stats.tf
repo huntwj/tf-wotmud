db4b4514bda6d1e0960e2f8d304e2f90b5f2c550
Wil Hunt <huntwj@gmail.com>
2015-12-07 00:46:57 -0800

    Add birthday, light, and peanuts support in stats.

--- stats.tf
+++ stats.tf
@@ -3,7 +3,9 @@
 ;;
 /loaded __tf_wotmud_stats__
 
-/def -mregexp -t"You are a \d+ year old (male|female) (human) (rogue|hunter|warrior|channeler)\.$" wot_stats_beginCapture = \
+/require tf-util/events.tf
+
+/def -mregexp -t"You are a \d+ year old (male|female) (human) (rogue|hunter|warrior|channeler)\.( It's your birthday today!)?$" wot_stats_beginCapture = \
     /let _sex=%{P1}%;\
     /let _class=%{P3}%;\
     /echo Beginning stats capture%;\
@@ -14,7 +16,7 @@
 /def -Evar_user_capturing_46_stats -mregexp -t"^Your height is \d+ feet, \d+ inches, and you weigh \d+\.\d+ lbs\.$" wot_stats_captureVitals = \
     /test 1
 
-/def -Evar_user_capturing_46_stats -mregexp -t"^You are carrying \d+\.\d+ lbs and wearing \d+\.\d+ lbs, (very light|fairly light)\.$" wot_stats_captureLoad = \
+/def -Evar_user_capturing_46_stats -mregexp -t"^You are carrying \d+\.\d+ lbs and wearing \d+\.\d+ lbs, (very light|fairly light|light|peanuts)\.$" wot_stats_captureLoad = \
     /test 1
 
 /def -Evar_user_capturing_46_stats -ag -mregexp -t"^Your base abilities are: Str:(\d+) Int:(\d+) Wil:(\d+) Dex:(\d+) Con:(\d+)\.$" wot_stats_captureStats = \
@@ -35,7 +37,8 @@
         /echo -aCyellow %{_line} (Stat Sum: %{_sum})%;\
     /else \
         /echo %{_line} (Stat Sum: %{_sum})%;\
-    /endif
+    /endif%;\
+    /util_fireEvent captured.abilities %{_str} %{_int} %{_wil} %{_dex} %{_con} %{_sum}
 
 /def -Evar_user_capturing_46_stats -mregexp -t"^Offensive bonus: \d+, Dodging bonus: \d+, Parrying bonus: \d+$" wot_stats_captureObDbPb = \
     /test 1
