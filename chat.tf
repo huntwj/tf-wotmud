4fb21ed212dcd23ea9cd49674131150250b5d316
Wil Hunt <huntwj@gmail.com>
2015-12-07 00:44:45 -0800

    Add bellows to chat limit.

--- chat.tf
+++ chat.tf
@@ -11,7 +11,7 @@
         /test isLimited := 0%;\
     /else \
         /let _success=%;\
-        /test _success := limit("-mregexp (tells you|chats|yells|speaks from the light|says|narrates)")%;\
+        /test _success := limit("-mregexp (tells you|chats|yells|speaks from the light|says|narrates|bellows)")%;\
         /test isLimited := _success%;\
     /endif
 
