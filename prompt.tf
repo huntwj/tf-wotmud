ef4ec1a8ee970c53de427f67b6ab6433d81fcc3c
Wil Hunt <huntwj@gmail.com>
2015-12-07 00:45:55 -0800

    Add support for riding in the prompt regexp

--- prompt.tf
+++ prompt.tf
@@ -7,18 +7,18 @@
 /test wot_movement_states := "Full|Fresh|Strong|Tiring|Winded|Weary|Haggard"
 /test wot_spell_states := "Bursting|Full|Strong|Good|Fading|Trickling"
 
-/eval /def -mregexp -h'PROMPT ^([*o] HP:(%{wot_health_states})( SP:(%{wot_spell_states}))? MV:(%{wot_movement_states})(( - (.*): (%{wot_health_states}))? - (.*): (%{wot_health_states}))? > )' wot_prompt_trigger = \
+/eval /def -mregexp -h'PROMPT ^([*o] (R )?HP:(%{wot_health_states})( SP:(%{wot_spell_states}))? MV:(%{wot_movement_states})(( - (.*): (%{wot_health_states}))? - (.*): (%{wot_health_states}))? > )' wot_prompt_trigger = \
     /wot_do_prompt
 
 /def wot_do_prompt = \
     /let _promptLine=%{P0}%;\
-    /let _charHealth=%{P2}%;\
-    /let _charPower=%{P4}%;\
-    /let _charMoves=%{P5}%;\
-    /let _tankDesc=%{P8}%;\
-    /let _tankHealth=%{P9}%;\
-    /let _targetDesc=%{P10}%;\
-    /let _targetHealth=%{P11}%;\
+    /let _charHealth=%{P3}%;\
+    /let _charPower=%{P5}%;\
+    /let _charMoves=%{P6}%;\
+    /let _tankDesc=%{P9}%;\
+    /let _tankHealth=%{P10}%;\
+    /let _targetDesc=%{P11}%;\
+    /let _targetHealth=%{P12}%;\
     /test util_setVar("char.health", {_charHealth})%;\
     /test util_setVar("char.power", {_charPower})%;\
     /test util_setVar("char.moves", {_charMoves})%;\
