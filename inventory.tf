bb7f6b23d8b7fbbba6417b6ed944c4b48fc7dac6
Wil Hunt <huntwj@gmail.com>
2015-12-07 03:52:47 -0800

    Add waterskin convenience stuffs.

--- inventory.tf
+++ inventory.tf
@@ -10,3 +10,16 @@
     gc light%;\
     hold light
 
+/alias fws /wot_fillWaterSkin
+/def wot_fillWaterSkin = \
+    /let _source=%{1-waterskin}%;\
+    /let _container=%{2-waterskin}%;\
+    fill %{_container} %{_source}
+
+/def dws = /wot_drinkWaterSkin
+/def wot_drinkWaterSkin = \
+    /let _container=%{2-waterskin}%;\
+    remove %{_container}%;\
+    /repeat -S 2 drink %{_container}%;\
+    wear %{_container}
+
